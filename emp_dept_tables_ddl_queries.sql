4eb50bac319f9e74d22ff20977964c5afa4036e7
RAVIADULLA <149261058+RAVIADULLA@users.noreply.github.com>
2025-08-29 22:25:09 -0400

    Add files via upload

---
+++ emp_dept_tables_ddl_queries.sql
@@ -0,0 +1,126 @@
+create table table2(id number)
+
+truncate table table1
+
+insert into table1 (id) values (null)
+
+select * from table1
+
+select * from table2
+
+select a.id
+from table1 a  join table2 b on a.id=b.id
+
+select a.id
+from table1 a left outer join table2 b on a.id=b.id
+
+select a.id
+from table1 a right outer join table2 b on a.id=b.id
+
+select a.id
+from table1 a full outer join table2 b on a.id=b.id
+
+select
+  CURRENT_USER ();
+
+  select * from emp;
+
+  select max(sal),b.deptno from emp_before_update b group by b.deptno;
+
+  select a.* from emp_before_update a
+  where sal in (select distinct(max(sal)) from emp_before_update b
+            where b.deptno=a.deptno group by b.deptno)
+
+update emp_before_update set sal=2000 where ename='FORD'
+
+select max(sal)
+from emp_before_update a where sal < (select max(sal) from emp_before_update)
+
+with cte as (
+select ename,empno,deptno,sal,rank() over (partition by deptno order by sal desc) r from emp_before_update
+) 
+select * from cte where r=1
+
+select * from (
+select ename,empno,deptno,sal,rank() over (partition by deptno order by sal desc) r from emp_before_update
+) a
+ where r=1
+
+select * from (
+select ename,empno,deptno,sal,row_number() over (partition by deptno order by sal desc) r from emp_before_update
+) a
+qualify r=1
+
+select * from database_raviadulla.schema_raviadulla.dual;
+
+
+select level as num
+from dual
+connect by level <=10;
+
+CREATE TABLE database_raviadulla.schema_raviadulla.dual
+(
+  DUMMY VARCHAR2(1)
+);
+
+insert into dual (dummy) values ('X');
+
+SELECT
+  LEVEL AS num
+FROM
+  DUAL CONNECT BY LEVEL <= 10;
+
+  SELECT LEVEL AS num
+FROM dual
+CONNECT BY PRIOR LEVEL  <= 10;
+
+select *
+from emp --where ename !='KING'
+start with mgr is null
+connect by prior empno=mgr
+
+CREATE or replace TABLE emp (
+  empno decimal(4,0) NOT NULL,
+  ename varchar(10) default NULL,
+  job varchar(9) default NULL,
+  mgr decimal(4,0) default NULL,
+  hiredate date default NULL,
+  sal decimal(7,2) default NULL,
+  comm decimal(7,2) default NULL,
+  deptno decimal(2,0) default NULL
+);
+
+INSERT INTO emp VALUES ('7369','SMITH','CLERK','7902','1980-12-17','800.00',NULL,'20');
+INSERT INTO emp VALUES ('7499','ALLEN','SALESMAN','7698','1981-02-20','1600.00','300.00','30');
+INSERT INTO emp VALUES ('7521','WARD','SALESMAN','7698','1981-02-22','1250.00','500.00','30');
+INSERT INTO emp VALUES ('7566','JONES','MANAGER','7839','1981-04-02','2975.00',NULL,'20');
+INSERT INTO emp VALUES ('7654','MARTIN','SALESMAN','7698','1981-09-28','1250.00','1400.00','30');
+INSERT INTO emp VALUES ('7698','BLAKE','MANAGER','7839','1981-05-01','2850.00',NULL,'30');
+INSERT INTO emp VALUES ('7782','CLARK','MANAGER','7839','1981-06-09','2450.00',NULL,'10');
+INSERT INTO emp VALUES ('7788','SCOTT','ANALYST','7566','1982-12-09','3000.00',NULL,'20');
+INSERT INTO emp VALUES ('7839','KING','PRESIDENT',NULL,'1981-11-17','5000.00',NULL,'10');
+INSERT INTO emp VALUES ('7844','TURNER','SALESMAN','7698','1981-09-08','1500.00','0.00','30');
+INSERT INTO emp VALUES ('7876','ADAMS','CLERK','7788','1983-01-12','1100.00',NULL,'20');
+INSERT INTO emp VALUES ('7900','JAMES','CLERK','7698','1981-12-03','950.00',NULL,'30');
+INSERT INTO emp VALUES ('7902','FORD','ANALYST','7566','1981-12-03','3000.00',NULL,'20');
+INSERT INTO emp VALUES ('7934','MILLER','CLERK','7782','1982-01-23','1300.00',NULL,'10');
+
+create or replace table dept(   
+  deptno     number(2,0),   
+  dname      varchar2(14),   
+  loc        varchar2(13),   
+  constraint pk_dept primary key (deptno)   
+);
+
+insert into dept
+values(10, 'ACCOUNTING', 'NEW YORK');
+insert into dept
+values(20, 'RESEARCH', 'DALLAS');
+insert into dept
+values(30, 'SALES', 'CHICAGO');
+insert into dept
+values(40, 'OPERATIONS', 'BOSTON');
+
+select * from dept;
+
+
