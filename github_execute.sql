e48abaec9915955adc0ad325972abdcc1558ff3b
RAVIADULLA <149261058+RAVIADULLA@users.noreply.github.com>
2025-08-31 21:41:45 -0400

    Update github_execute.sql

--- github_execute.sql
+++ github_execute.sql
@@ -1,1 +1,10 @@
-execute immediate from @RAVIADULLA/Snowflake_files/edit/main/github_execute.sql
+CREATE or replace TABLE emp_bkp (
+  empno decimal(4,0) NOT NULL,
+  ename varchar(10) default NULL,
+  job varchar(9) default NULL,
+  mgr decimal(4,0) default NULL,
+  hiredate date default NULL,
+  sal decimal(7,2) default NULL,
+  comm decimal(7,2) default NULL,
+  deptno decimal(2,0) default NULL
+);
