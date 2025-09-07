9763cb43619c0845598b11f5be9d10f5b8bb74c1
RAVIADULLA <149261058+RAVIADULLA@users.noreply.github.com>
2025-09-07 13:06:03 -0400

    Update json_parquet_flatten.sql

--- json_parquet_flatten.sql
+++ json_parquet_flatten.sql
@@ -2,17 +2,32 @@
 
 SELECT * FROM "DATABASE_RAVIADULLA"."SCHEMA_RAVIADULLA"."FIRST_JASON";
 
+describe table FIRST_JASON;
+
+select * from FIRST_JASON;
+
+select c1:accountStatus from FIRST_JASON;
+
+select 
+c1:accountStatus:createdAt,
+c1:accountStatus:createdAt as create_date,
+c1:accountStatus:createdAt::date as create_date,
+c1:accountStatus:isActive,
+c1:accountStatus:isActive::string as isActive,
+c1:accountStatus:lastLogin
+from FIRST_JASON;
+
 SELECT
---C1:activityLogs,
+C1:activityLogs,
 a1.value:activity::string as activity,
 a1.value:timestamp::timestamp as timestamp_value
 FROM
 "DATABASE_RAVIADULLA"."SCHEMA_RAVIADULLA"."FIRST_JASON",
-table (flatten (C1:activityLogs)) a1  -- we have another object in the object we can use one more flatten (multiline)
+table (flatten (C1:activityLogs)) a1  -- if we have another object in the object we can use one more flatten (multiline)
 
 --[] -- array
 --{} -- object
--- within object key name and value pair
+-- within object key name and value pair are available
 
 
 SELECT
@@ -35,3 +50,4 @@ VARIANT_COL:carb::string as carb,
 VARIANT_COL:disp::string as disp,
 VARIANT_COL:model::string as model
 from DATABASE_RAVIADULLA.SCHEMA_RAVIADULLA.MTCARS
+
