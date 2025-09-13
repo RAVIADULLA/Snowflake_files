859772f0bc425b7c14277c2b011144e07e5f11ee
RAVIADULLA <149261058+RAVIADULLA@users.noreply.github.com>
2025-09-13 01:32:21 -0400

    Update SQL_Interview_questions.sql

--- SQL_Interview_questions.sql
+++ SQL_Interview_questions.sql
@@ -46,6 +46,61 @@ FROM    employees e
 START WITH e.emp_id = :emp_id
 CONNECT BY PRIOR e.manager_id = e.emp_id;
 
+select *
+from emp e
+where e.sal > (select cast(avg(sal) as int) as avgsal from emp );
+
+with avg_sal (avgsal) as 
+(select cast(avg(sal) as int) as avgsal from emp )
+select *
+from emp e, avg_sal avg
+where e.sal > avg.avgsal;
+
+select e.deptno, cast(sum(sal) as int) sumsal
+from emp e
+group by deptno
+
+select cast(avg(sumsal) as int) avg
+from 
+    (select e.deptno, cast(sum(sal) as int) sumsal
+    from emp e
+    group by deptno) as total_avg
+
+
+select * from 
+(select e.deptno, cast(sum(sal) as int) sumsal_dept
+from emp e
+group by deptno) sum_sal_dept
+join
+(select cast(avg(sumsal) as int) avgsumsal
+from 
+    (select e.deptno, cast(sum(sal) as int) sumsal
+    from emp e
+    group by deptno) as total_avg) as totalavg
+on sum_sal_dept.sumsal_dept > totalavg.avgsumsal
+
+
+with sum_sal_dept (deptno,sumsal_dept) as 
+    (select e.deptno, cast(sum(sal) as int) sumsal_dept
+    from emp e
+    group by deptno),
+    totalavg (sum_avg)  as 
+    (select cast(avg(sumsal_dept) as int) from sum_sal_dept)
+select * from sum_sal_dept , totalavg where sumsal_dept > sum_avg
+
+select e.*,
+row_number() over (partition by deptno order by sal desc) as rn
+from emp e
+qualify rn=2
+
+select e.*,
+lag(sal) over (partition by deptno order by deptno desc) as previous_sal, -- default value is null
+lag(sal,2,0) over (partition by deptno order by deptno desc) as previous_2_sal, -- default values 0
+lead(sal) over (partition by deptno order by deptno desc) as previous_sal, -- default value is null
+lead(sal,2,0) over (partition by deptno order by deptno desc) as previous_2_sal, -- default values 0
+from emp e
+
+
 
 
 
